e3e2be19bf5246a373c1529357be3ca153dcc8e8
FelippeSalvoDev <felippesalvo.dev@gmail.com>
2026-02-12 20:22:09 -0300

    feat: implementação da movimentação da peça (direções, colisão)

--- Tetris-sharp/Program.cs
+++ Tetris-sharp/Program.cs
@@ -28,6 +28,45 @@ class JogoTetris
             pecaAtual = gerarPeca();
             posicaoX = 0;
             posicaoY = colunas / 2 - 1; //Sempre gera a peça no meio 
+
+            if (ColidiuPeca(pecaAtual, posicaoX, posicaoY))
+            {
+                Console.Clear();
+                Console.WriteLine("FIM DE JOGO");
+                break;
+            }
+
+            bool caiu = false;
+
+            while (!caiu)
+            {
+                Console.Clear();
+
+                Console.WriteLine("A = Esquerda | D = Direita | S = Descer");
+
+                var tecla = Console.ReadKey(true).Key;
+
+                switch (tecla)
+                {
+                    case ConsoleKey.A:
+                        if (!ColidiuPeca(pecaAtual, posicaoX, posicaoX))
+                            posicaoY--; //Move para esquerda
+                        break;
+                    case ConsoleKey.D:
+                        if (!ColidiuPeca(pecaAtual, posicaoX, posicaoX))
+                            posicaoY++; //Move para direita
+                        break;
+                    case ConsoleKey.S:
+                        if (!ColidiuPeca(pecaAtual, posicaoX, posicaoX))
+                            posicaoX++; //Move para baixo
+                        else
+                        {
+                            ColocarPeca();
+                            caiu = true;
+                        }
+                        break;
+                }
+            }
         }
     }
 
@@ -66,6 +105,44 @@ class JogoTetris
                     int py = posicaoY + j;
                     if (px == x && py == y)
                         return true;
+                }
+            }
+        }
+    }
+
+    bool ColidiuPeca(int[,] peca, int x, int y)
+    {
+        for (int i = 0; i < peca.GetLength(0); i++)
+        {
+            for (int j = 0; j < peca.GetLength(1); j++)
+            {
+                if (peca[i, j] == 1)
+                {
+                    int px = x + 1;
+                    int py = y + 1;
+
+                    if (px < 0 || px >= linhas || py < 0 || py >= colunas) //Garante que a peça não escape do tabuleiro
+                        return true;
+                    if (tabuleiro[px, py] == 1)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void ColocarPeca()
+    {
+        for (int i = 0; i < pecaAtual.GetLength(0); i++)
+        {
+            for (int j = 0; j < pecaAtual.GetLength(1); j++)
+            {
+                if ((pecaAtual[i, j] == 1))
+                {
+                    int px = posicaoX + i;
+                    int py = posicaoY + j;
+                    if (px >= 0 && px < linhas && py >= 0 && py < colunas) //Garante que a peça não escape do tabuleiro
+                        tabuleiro[px, py] = 1; //Coloca a peça
                 }
             }
         }
