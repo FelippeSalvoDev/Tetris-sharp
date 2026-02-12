885fe1bd4bafb3d5a34810607f5dc5364c63b864
FelippeSalvoDev <felippesalvo.dev@gmail.com>
2026-02-12 19:32:25 -0300

    feat: estrutura inicial do projeto Tetris em console

---
+++ Tetris-sharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class JogoTetris
+{
+    private int linhas = 20;
+    private int colunas = 10;
+
+    private int[,] tabuleiro;
+    private int[,] pecaAtual;
+
+    private int posicaoX;
+    private int posicaoY;
+
+    private int pontos = 0;
+    private string nomeJogador;
+
+    public void Comecar()
+    {
+        tabuleiro = new int[linhas, colunas];
+
+        Console.WriteLine("Digite seu nome: ");
+        nomeJogador = Console.ReadLine();
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        JogoTetris jogo = new JogoTetris();
+        jogo.Comecar();
+    }
+}
